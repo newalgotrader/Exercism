e645074d00c3aca1bcbbebbe4996b62c01202e3f
Nick DeLuca <nick@newalgotrader.com>
2020-05-30 19:12:00 -0500

    add c armstrong numbers

---
+++ c/armstrong-numbers/src/armstrong_numbers.c
@@ -0,0 +1,39 @@
+#include "armstrong_numbers.h"
+
+static int number_of_digits(int);
+static int power(int, int);
+
+int is_armstrong_number(int candidate) {
+  int sum = 0;
+  int result = candidate;
+  int digits = number_of_digits(candidate);
+
+  while (result > 0) {
+    sum += power(result % 10, digits);
+    result /= 10;
+  }
+
+  return sum == candidate;
+}
+
+static int number_of_digits(int number) {
+  int digits = 0;
+
+  while(number > 0) {
+    number /= 10;
+    ++digits;
+  }
+
+  return digits;
+}
+
+static int power(int base, int power) {
+  int result = 1;
+
+  while(power) {
+    result *= base;
+    --power;
+  }
+
+  return result;
+}
